c7c0903f8078ff5c74a91823595ae14f00526a62
al-furkan <furkanf4336@gmail.com>
2025-07-20 09:50:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import MemoryMatchGame from "./Pages/MemoryMatchGame";
+
+
+function App() {
+  return <MemoryMatchGame />;
+}
+
+export default App;
---
+++ src/Pages/MemoryMatchGame.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MemoryMatchGame = () => {
+  return (
+    <div className="memory-match-game">
+        Hello
+    </div>
+  );
+};
+
+export default MemoryMatchGame;
